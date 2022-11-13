2ace73aaa69f26d458cc088a08cb9f1ceb78f600
Fernanda leite <fernandacarolineleite360@gmail.com>
2022-11-13 16:33:03 -0300

    feat: create todo app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,57 @@
+import { useState } from "react";
+import { PlusCircle } from "phosphor-react";
+
+import { Header } from "./components/Header";
+import { Empty } from "./components/Empty";
+import { Task } from "./components/Task";
+
+import styles from "./styles/App.module.css";
+
 function App() {
+  const [tasks, setTasks] = useState(1);
+
   return (
-    <h1>Hello todo</h1>
+    <div className={styles.todoApp}>
+      <Header />
+
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <form>
+            <input type="text" placeholder="Adicione uma nova tarefa" />
+
+            <button type="submit">
+              <span>Criar</span>
+
+              <PlusCircle size={16} color="#F2F2F2" />
+            </button>
+          </form>
+
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <div className={styles.createdTasks}>
+                <strong>Tarefas criadas</strong>
+                <span>0</span>
+              </div>
+
+              <div className={styles.completedTasks}>
+                <strong>Concluídas</strong>
+                <span>0</span>
+              </div> 
+            </div>
+
+            {tasks ? (
+              <div className={styles.tasks}>
+                <Task />
+                <Task />
+                <Task />
+              </div>
+            ) : (
+              <Empty />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
